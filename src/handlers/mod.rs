886e6427db86b89e6e01e3d787146cf35f0b1ff5
Ivan P <25914822+userepo@users.noreply.github.com>
2026-05-07 20:56:59 -0500

    Stage 3 - connect endpoints with DAO (#3)
    * refactored DAO to use query!
    * add inner handlers, which use DAO traits
    * add app state for Axum
    * use inner handlers in the handlers; make API endpoints more conventional

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,81 +1,71 @@
-use crate::models::*;
-use axum::{
-    http::StatusCode,
-    response::IntoResponse, Json};
+use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+use axum::extract::Path;
+use crate::{AppState, models::*};
 
+mod handlers_inner;
+
+impl IntoResponse for handlers_inner::HandlerError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            handlers_inner::HandlerError::BadRequest(msg) => {
+                (StatusCode::BAD_REQUEST, msg).into_response()
+            }
+            handlers_inner::HandlerError::InternalError(msg) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+            }
+        }
+    }
+}
 
 // ---- Questions ----
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    let question_detail = QuestionDetail {
-        question_uuid: "8f3c2c4e-9b6a-4d7e-9c1e-2e4a1f7d9b32".to_string(),
-        title: question.title,
-        description: question.description,
-        created_at: "2026-05-02 15:35:10.000".to_string(),
-    };
-
-    (StatusCode::CREATED, Json(question_detail))
+pub async fn create_question(
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question): Json<Question>
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::create_question(question, questions_dao.as_ref())
+        .await
+        .map(Json)
 }
 
-pub async fn read_questions() -> impl IntoResponse {
-    let question_detail1 = QuestionDetail {
-        question_uuid: "8f3c2c4e-9b6a-4d7e-9c1e-2e4a1f7d9b32".to_string(),
-        title: "Newly Created Question 1".to_string(),
-        description: "My Description 1".to_string(),
-        created_at: "2026-05-02 15:35:10.000".to_string(),
-    };
-    let question_detail2 = QuestionDetail {
-        question_uuid: "d1a7e0f4-3c8b-4c2e-9f55-0b7e2c4a8f91".to_string(),
-        title: "Newly Created Question 2".to_string(),
-        description: "My Description 2".to_string(),
-        created_at: "2026-05-02 15:40:15.000".to_string(),
-    };
-
-    let question_details = vec![question_detail1, question_detail2];
-
-    (StatusCode::OK, Json(question_details))
+pub async fn read_questions(
+    State(AppState { questions_dao, .. }): State<AppState>
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::read_questions(questions_dao.as_ref())
+        .await
+        .map(Json)
 }
 
-pub async fn delete_question(Json(question_id): Json<QuestionId>) {
-    //let question_uuid = question_id.question_uuid;
-    ()
+pub async fn delete_question(
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Path(question_uuid): Path<String>,   // extracts {id} from URL
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::delete_question(question_uuid, questions_dao.as_ref()).await
 }
 
 // ---- Answers ----
 
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
-    let answer_detail = AnswerDetail {
-        answer_uuid: "4b92e7d1-6f3a-4c0b-8c44-1e0f9a3d7b20".to_string(),
-        question_uuid: answer.question_uuid,
-        content: answer.content,
-        created_at: "2026-05-02 18:40:50.700".to_string(),
-    };
-
-    (StatusCode::CREATED, Json(answer_detail))
+pub async fn create_answer(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer): Json<Answer>
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::create_answer(answer, answers_dao.as_ref())
+        .await
+        .map(Json)
 }
 
-pub async fn read_answers(Json(question_id): Json<QuestionId>) -> impl IntoResponse {
-
-    let answer_detail1 = AnswerDetail {
-        answer_uuid: "4b92e7d1-6f3a-4c0b-8c44-1e0f9a3d7b20".to_string(),
-        question_uuid: question_id.question_uuid,
-        content: "Test question 1".to_string(),
-        created_at: "2026-05-02 18:50:29.000".to_string(),
-    };
-
-    let answer_detail2 = AnswerDetail {
-        answer_uuid: "4b92e7d1-6f3a-4c0b-8c44-1e0f9a3d7b20".to_string(),
-        question_uuid: "d1a7e0f4-3c8b-4c2e-9f55-0b7e2c4a8f91".to_string(),
-        content: "Test question 2".to_string(),
-        created_at: "2026-05-02 18:55:20.000".to_string(),
-    };
-
-    let answer_details = vec![answer_detail1, answer_detail2];
-
-    (StatusCode::OK, Json(answer_details))
+pub async fn read_answers(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Path(question_uuid): Path<String>,   // extracts {id} from URL
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::read_answers(question_uuid, answers_dao.as_ref())
+        .await
+        .map(Json)
 }
 
-pub async fn delete_answer(Json(answer_id): Json<AnswerId>) {
-    //let answer_uuid = answer_id.answer_uuid;
-    ()
+pub async fn delete_answer(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Path(answer_uuid): Path<String>
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::delete_answer(answer_uuid, answers_dao.as_ref()).await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-
 use std::{env, time::Duration};
 
-use log::{debug, error, info, trace, warn};
+//use log::{debug, error, info, trace, warn};
 use pretty_env_logger;
+
+use std::sync::Arc;
 
 use axum::{
     routing::{delete, get, post},
@@ -11,6 +12,10 @@ use axum::{
 
 use dotenvy::dotenv;
 
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
+};
 use sqlx::postgres::PgPoolOptions;
 
 mod handlers;
@@ -18,7 +23,12 @@ mod models;
 mod persistance;
 
 use handlers::*;
-//use models::*;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -28,20 +38,27 @@ async fn main() {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    let _pool = PgPoolOptions::new()
+    let pool = PgPoolOptions::new()
         .max_connections(5)
         .acquire_timeout(Duration::from_secs(15))
         .connect(database_url.as_str())
 	.await
         .expect("Failed to create Postgres connection pool!");
-    
+
+    let questions_dao = QuestionsDaoImpl::new(pool.clone());
+    let answers_dao = AnswersDaoImpl::new(pool);
+
+    let app_state = AppState {
+    	questions_dao: Arc::new(questions_dao), 
+    	answers_dao: Arc::new(answers_dao) 
+    };
+
     let app = Router::new()
-        .route("/question", post(create_question))
-        .route("/questions", get(read_questions))
-        .route("/question", delete(delete_question))
-        .route("/answer", post(create_answer))
-        .route("/answers", get(read_answers))
-        .route("/answer", delete(delete_answer));
+        .route("/questions", post(create_question).get(read_questions))
+        .route("/questions/{id}", delete(delete_question))
+        .route("/answers", post(create_answer))
+        .route("/answers/{id}", get(read_answers).delete(delete_answer))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
